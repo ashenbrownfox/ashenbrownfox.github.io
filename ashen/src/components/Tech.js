dea1fcd048536708ed7175bedab1dc1da55defd3
Ailun Shen <newhoopsdynasty@gmail.com>
2023-05-12 15:44:41 -0400

    Refactor Tech.js even more, to loop

--- ashen/src/components/Tech.js
+++ ashen/src/components/Tech.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import s1 from './../images/html.png';
-import s2 from './../images/csharp.png'; 
-import s3 from './../images/bootstrap.png'; 
-import s4 from './../images/git.png'; 
-import s5 from './../images/javascript.png'; 
-import s6 from './../images/java.png'; 
-import s7 from './../images/node.png'; 
-import s8 from './../images/php.png'; 
-import s9 from './../images/rails.png'; 
-import s10 from './../images/sql.png'; 
-import s11 from './../images/jquery.png'; 
-import s12 from './../images/DNET.jpg'; 
+import s2 from './../images/csharp.png';
+import s3 from './../images/bootstrap.png';
+import s4 from './../images/git.png';
+import s5 from './../images/javascript.png';
+import s6 from './../images/java.png';
+import s7 from './../images/node.png';
+import s8 from './../images/php.png';
+import s9 from './../images/rails.png';
+import s10 from './../images/sql.png';
+import s11 from './../images/jquery.png';
+import s12 from './../images/DNET.jpg';
+
+const skills = [
+  { image: s1 },
+  { image: s2 },
+  { image: s3 },
+  { image: s4 },
+  { image: s5 },
+  { image: s6 },
+  { image: s7 },
+  { image: s8 },
+  { image: s9 },
+  { image: s10 },
+  { image: s11 },
+  { image: s12 },
+];
 
 const Tech = () => {
   return (
@@ -18,88 +33,20 @@ const Tech = () => {
       <div className="container skill-container">
         <header>
           <h2>Technology Proficiency</h2>
-          <div className="title-icon"> <i className="fa fa-magic"></i> </div>
+          <div className="title-icon">
+            <i className="fa fa-magic"></i>
+          </div>
         </header>
 
         <div className="row">
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s1} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s2} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s3} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s4} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s5} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s6} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s7} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s8} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src= {s9} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src= {s10} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src={s11} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-            <img src={s12} className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
+          {skills.map((skill, index) => (
+            <div className="3u 12u(mobile) skill" key={index}>
+              <section className="box style1">
+                <img src={skill.image} className="skill-image" alt="" />
+                <p></p>
+              </section>
+            </div>
+          ))}
         </div>
       </div>
     </section>
