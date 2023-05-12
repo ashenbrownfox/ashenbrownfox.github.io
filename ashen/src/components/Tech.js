cf7fb11380d6d0efff8fbb919404a255cb704f53
Ailun Shen <newhoopsdynasty@gmail.com>
2023-05-12 15:42:04 -0400

    Refactor tech so images are showing

--- ashen/src/components/Tech.js
+++ ashen/src/components/Tech.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import s1 from './../images/html.png';
+import s2 from './../images/csharp.png'; 
+import s3 from './../images/bootstrap.png'; 
+import s4 from './../images/git.png'; 
+import s5 from './../images/javascript.png'; 
+import s6 from './../images/java.png'; 
+import s7 from './../images/node.png'; 
+import s8 from './../images/php.png'; 
+import s9 from './../images/rails.png'; 
+import s10 from './../images/sql.png'; 
+import s11 from './../images/jquery.png'; 
+import s12 from './../images/DNET.jpg'; 
 
 const Tech = () => {
   return (
@@ -12,52 +24,25 @@ const Tech = () => {
         <div className="row">
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/html.png" className="skill-image" alt="" />
+              <img src={s1} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/css.png" className="skill-image" alt="" />
+              <img src={s2} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/javascript.png" className="skill-image" alt="" />
+              <img src={s3} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/jquery.png" className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src="images/csharp.png" className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src="images/java.png" className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src="images/sql.png" className="skill-image" alt="" />
-              <p></p>
-            </section>
-          </div>
-          <div className="3u 12u(mobile) skill">
-            <section className="box style1">
-              <img src="images/git.png" className="skill-image" alt="" />
+              <img src={s4} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
@@ -66,25 +51,52 @@ const Tech = () => {
         <div className="row">
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/node.png" className="skill-image" alt="" />
+              <img src={s5} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/rails.png" className="skill-image" alt="" />
+              <img src={s6} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-              <img src="images/php.png" className="skill-image" alt="" />
+              <img src={s7} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
           <div className="3u 12u(mobile) skill">
             <section className="box style1">
-            <img src="images/bootstrap.png" className="skill-image" alt="" />
+              <img src={s8} className="skill-image" alt="" />
+              <p></p>
+            </section>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="3u 12u(mobile) skill">
+            <section className="box style1">
+              <img src= {s9} className="skill-image" alt="" />
+              <p></p>
+            </section>
+          </div>
+          <div className="3u 12u(mobile) skill">
+            <section className="box style1">
+              <img src= {s10} className="skill-image" alt="" />
+              <p></p>
+            </section>
+          </div>
+          <div className="3u 12u(mobile) skill">
+            <section className="box style1">
+              <img src={s11} className="skill-image" alt="" />
+              <p></p>
+            </section>
+          </div>
+          <div className="3u 12u(mobile) skill">
+            <section className="box style1">
+            <img src={s12} className="skill-image" alt="" />
               <p></p>
             </section>
           </div>
