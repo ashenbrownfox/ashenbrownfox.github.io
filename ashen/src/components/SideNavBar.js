55de9fd6d9169bdd4244cae0a2ab28a750ffe3f8
Ailun Shen <newhoopsdynasty@gmail.com>
2023-05-12 15:25:08 -0400

    Refactor side nav bar to function based

--- ashen/src/components/SideNavBar.js
+++ ashen/src/components/SideNavBar.js
@@ -1,39 +1,75 @@
-import React, {Component} from 'react';
-import profilepic from './../css/images/avatar1.jpg';
+import React from 'react';
+import pfp from  './../images/avatar1.jpg';
 
-class SideNavBar extends Component {
-  render(){
-    return (
-      <div>
-        <div id="header">
-          <div class="top">
-              <div id="logo">
-                <span class="image avatar48"><img src={profilepic} alt="" /></span>
-                <h1 id="title">Ailun Shen</h1>
-                <p>Software Developer</p>
-              </div>
-              <nav id="nav">
-                <ul>
-                  <li><a href="#top" id="top-link" class="skel-layers-ignoreHref"><span class="icon fa-home">Intro</span></a></li>
-                  <li><a href="#about" id="about-link" class="skel-layers-ignoreHref"><span class="icon fa-user">About Me</span></a></li>
-                  <li><a href="#skills" id="skills-link" class="skel-layers-ignoreHref"><span class="icon fa-magic">Skills</span></a></li>
-                  <li><a href="#portfolio" id="portfolio-link" class="skel-layers-ignoreHref"><span class="icon fa-th">Portfolio</span></a></li>
-                  <li><a href="#contact" id="contact-link" class="skel-layers-ignoreHref"><span class="icon fa-envelope">Contact</span></a></li>
-                </ul>
-              </nav>
+const SideNavBar = () => {
+  return (
+    <div>
+      <div id="header">
+        <div className="top">
+          <div id="logo">
+            <span className="image avatar48">
+              <img src={pfp} alt="pfp" />
+            </span>
+            <h1 id="title">Ailun Shen</h1>
+            <p>Software Engineer</p>
           </div>
-          <div class="bottom">
-              <ul class="icons">
-                <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                <li><a href="https://github.com/ashenbrownfox" class="icon fa-github"><span class="label">Github</span></a></li>
-                <li><a href="https://www.linkedin.com/in/ailun-shen-4b2026109" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
-                <li><a href="mailto:ailun.shen@wmich.edu" class="icon fa-envelope"><span class="label">Email</span></a></li>
-              </ul>
-          </div>
-
-          </div>
+          <nav id="nav">
+            <ul>
+              <li>
+                <a href="#top" id="top-link" className="skel-layers-ignoreHref">
+                  <span className="icon fa-home">Intro</span>
+                </a>
+              </li>
+              <li>
+                <a href="#about" id="about-link" className="skel-layers-ignoreHref">
+                  <span className="icon fa-user">About Me</span>
+                </a>
+              </li>
+              <li>
+                <a href="#skills" id="skills-link" className="skel-layers-ignoreHref">
+                  <span className="icon fa-magic">Skills</span>
+                </a>
+              </li>
+              <li>
+                <a href="#portfolio" id="portfolio-link" className="skel-layers-ignoreHref">
+                  <span className="icon fa-th">Portfolio</span>
+                </a>
+              </li>
+              <li>
+                <a href="#contact" id="contact-link" className="skel-layers-ignoreHref">
+                  <span className="icon fa-envelope">Contact</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="bottom">
+          <ul className="icons">
+            <li>
+              <a href="#" className="icon fa-twitter">
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/ashenbrownfox" className="icon fa-github">
+                <span className="label">Github</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/ailun-shen" className="icon fa-linkedin">
+                <span className="label">Linkedin</span>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:ailun.shen@wmich.edu" className="icon fa-envelope">
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default SideNavBar;
