8653ad18b78fdb3e7cdd5e81efc71a94bc3c0e56
Ailun Shen <newhoopsdynasty@gmail.com>
2023-05-13 12:36:48 -0400

    Updates

---
+++ precache-manifest.5472c42bfc5aaeebb723cdf2da380726.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459b1a05cedab6e8d7b1be164b2dd1dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d322ef98da78661289f",
+    "url": "/static/css/main.585a4dae.chunk.css"
+  },
+  {
+    "revision": "5d00f4bf3d52151dacda",
+    "url": "/static/js/2.3b83162e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b83162e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d322ef98da78661289f",
+    "url": "/static/js/main.76e6b7f0.chunk.js"
+  },
+  {
+    "revision": "12eff4b2339433b04f99",
+    "url": "/static/js/runtime-main.803abde8.js"
+  },
+  {
+    "revision": "1a632d2b39f3275752f2c4b3c73477a3",
+    "url": "/static/media/angular.1a632d2b.png"
+  },
+  {
+    "revision": "a410270b6b27339300d6d0d77bbbc6be",
+    "url": "/static/media/avatar1.a410270b.jpg"
+  },
+  {
+    "revision": "b03bf462d1840b3af61a169b288583d5",
+    "url": "/static/media/aws.b03bf462.png"
+  },
+  {
+    "revision": "c45bd021cbcef73abfd03fd2b93aa02e",
+    "url": "/static/media/java.c45bd021.png"
+  },
+  {
+    "revision": "f5d8e6aecfb22f80bfcc92dc54e056c9",
+    "url": "/static/media/node.f5d8e6ae.png"
+  },
+  {
+    "revision": "b0e594144f6569f9a9d4683ebb9f2da7",
+    "url": "/static/media/overlay.b0e59414.png"
+  },
+  {
+    "revision": "94d7c6f7aff9444ba970bccb9276b68d",
+    "url": "/static/media/p1.94d7c6f7.jpg"
+  },
+  {
+    "revision": "4ad678e16d88aa7c5155c6340db88064",
+    "url": "/static/media/p2.4ad678e1.jpg"
+  },
+  {
+    "revision": "0f978ead34f7c7c78fa43a326dc38237",
+    "url": "/static/media/p3.0f978ead.jpg"
+  },
+  {
+    "revision": "d585f3cebb7d5da74c4ad1c275d594b4",
+    "url": "/static/media/p6.d585f3ce.jpg"
+  },
+  {
+    "revision": "ccecd27dc33732137a0b5200f616e4a2",
+    "url": "/static/media/p7.ccecd27d.jpg"
+  },
+  {
+    "revision": "7aa370f5e86f5bb016d51e4bdb2edaf4",
+    "url": "/static/media/php.7aa370f5.png"
+  },
+  {
+    "revision": "20c5f6ed27e01e5ca6f67ca4e4b23ae1",
+    "url": "/static/media/py.20c5f6ed.png"
+  },
+  {
+    "revision": "d2cbe9901351debebde49b66e675b272",
+    "url": "/static/media/react.d2cbe990.png"
+  },
+  {
+    "revision": "4aaff929af8a78f0a5e3c1b27a0b6725",
+    "url": "/static/media/redis.4aaff929.svg"
+  },
+  {
+    "revision": "db767259a291c24ce95816fac91fdd94",
+    "url": "/static/media/sql.db767259.png"
+  },
+  {
+    "revision": "f64cc35ebe411b6cacff1317d68e7b33",
+    "url": "/static/media/top-bg2.f64cc35e.jpg"
+  }
+]);
